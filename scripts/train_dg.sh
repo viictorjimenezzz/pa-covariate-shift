2a606e3951c424a03fdc78327f08df826ffcbb76
Alessandro Torcinovich <torci.dev@gmail.com>
2023-08-10 17:19:48 +0200

    Make script run

--- scripts/train_dg.sh
+++ scripts/train_dg.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+set -euo pipefail
 
-python src/train_dg.py \
-    --cfg job \ 
-    experiment=dg/train_dg_erm \
-    # data/adv/model@data.model=weak,robust \
-    # data/adv/attack@data.attack=PGD \
-    # data.attack.epsilons=0.0314,0.0627,0.1255 \
-    # data.attack.adversarial_ratio=0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0 \
+
+CUDA_VISIBLE_DEVICES=2,3,4,5 python src/train_dg.py \
+    experiment=dg/train_dg_erm
+    # --cfg=job \
     #logger=wandb
