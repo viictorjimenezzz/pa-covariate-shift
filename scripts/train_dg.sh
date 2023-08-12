3a98dfcf6ec229001db32eb4341271ecbc13dcf5
Alessandro Torcinovich <torci.dev@gmail.com>
2023-08-12 10:53:50 +0200

    Change arguments

--- scripts/train_dg.sh
+++ scripts/train_dg.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 
 CUDA_VISIBLE_DEVICES=2,3,4,5 python src/train_dg.py \
-    experiment=dg/train_dg_erm
-    # --cfg=job \
-    #logger=wandb
+    experiment=dg/train_dg_erm \
+    trainer.min_epochs=100 \
+    trainer.max_epochs=100 \
+    name=diagvib_weak \
+    logger=wandb
