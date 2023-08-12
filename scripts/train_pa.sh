b6a9fda442b7b1bd2e31bfd33a086cea58c5dae5
Alessandro Torcinovich <torci.dev@gmail.com>
2023-08-12 10:54:07 +0200

    Move commented argument

--- scripts/train_pa.sh
+++ scripts/train_pa.sh
@@ -2,10 +2,10 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
 
 python src/train_pa.py \
-    # --cfg job \
     experiment=adv/optimize_beta \
     data/adv/model@data.model=weak,robust \
     data/adv/attack@data.attack=PGD \
     data.attack.epsilons=0.0314,0.0627,0.1255 \
     data.attack.adversarial_ratio=0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0 \
-    # logger=wandb
+    logger=wandb
+    # --cfg job \
