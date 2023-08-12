83d7009b1adaaa135ad73883a7cc7a32fc10b0d6
Alessandro Torcinovich <torci.dev@gmail.com>
2023-08-12 10:58:21 +0200

    Comment out unused code

--- src/models/diagvib_pa_module.py
+++ src/models/diagvib_pa_module.py
@@ -35,9 +35,9 @@ class PosteriorAgreementModule(pl.LightningModule):
         self.afr_true = Accuracy(task="multiclass", num_classes=10)
         self.afr_pred = Accuracy(task="multiclass", num_classes=10)
 
-        self.betas = np.linspace(0, 10, 100).tolist()
-        self.training_step_outputs_1 = []
-        self.training_step_outputs_2 = []
+        # self.betas = np.linspace(0, 10, 100).tolist()
+        # self.training_step_outputs_1 = []
+        # self.training_step_outputs_2 = []
 
     def model_step(self, batch: Any):
         # QUICK FIX for projection. TODO: implement a better version
@@ -83,8 +83,8 @@ class PosteriorAgreementModule(pl.LightningModule):
                 logger=True,
             )
 
-        self.training_step_outputs_1.append(o1)
-        self.training_step_outputs_2.append(o2)
+        # self.training_step_outputs_1.append(o1)
+        # self.training_step_outputs_2.append(o2)
         self.kernel(o1, o2)
 
         values = {
@@ -103,41 +103,50 @@ class PosteriorAgreementModule(pl.LightningModule):
 
         return loss
 
-    def on_train_epoch_end(self):
-        if self.current_epoch == 0:
-            PAs = []
-            log_PAs = []
-            for beta in self.betas:
-                kernel = PosteriorAgreementKernel(beta=beta)        
-                kernel.to(self.kernel.beta.device)
-                kernel.eval()
-                with torch.no_grad():
-                    for o1,o2 in zip(self.training_step_outputs_1,self.training_step_outputs_2):
-                        o1.to(kernel.beta.device)
-                        o2.to(kernel.beta.device)
-                        kernel(o1,o2)
-                    PAs.append(kernel.posterior().cpu().numpy())
-                    log_PAs.append(kernel.log_posterior().cpu().numpy())
-                    
-            logger.log({
-                "all_betas": wandb.Table(data=self.betas),
-                "all_PAs": wandb.Table(data=PAs),
-                "all_log_PAs": wandb.Table(data=log_PAs)
-            })
-            
-            # fig1, ax1 = plt.subplots()
-            # ax1.plot(self.betas, PAs)
-            # ax1.set_xlabel("beta")
-            # ax1.set_ylabel("PA")
+    # def on_train_epoch_end(self):
+    #     if self.current_epoch == 0:
+    #         PAs = []
+    #         log_PAs = []
+    #         for beta in self.betas:
+    #             kernel = PosteriorAgreementKernel(beta0=beta)
+    #             kernel.to(self.kernel.beta.device)
+    #             kernel.eval()
+    #             with torch.no_grad():
+    #                 for o1, o2 in zip(
+    #                     self.training_step_outputs_1,
+    #                     self.training_step_outputs_2,
+    #                 ):
+    #                     o1.to(kernel.beta.device)
+    #                     o2.to(kernel.beta.device)
+    #                     kernel(o1, o2)
+    #                 PAs.append(kernel.posterior().cpu().numpy())
+    #                 log_PAs.append(kernel.log_posterior().cpu().numpy())
 
-            # fig2, ax2 = plt.subplots()
-            # ax2.plot(self.betas, log_PAs)
-            # ax2.set_xlabel("Beta")
-            # ax2.set_ylabel("log_PA")
-            # logger = self.logger.experiment
-            # logger.log({"PA_vs_Beta":fig1})
-            # logger.log({"logPA_vs_Beta": fig2})
-                        
+    #         values = {
+    #             "all_betas": self.betas,
+    #             "all_logPA": PAs,
+    #             "all_PA": log_PAs,
+    #         }
+    #         self.log_dict(
+    #             values,
+    #             prog_bar=True,
+    #             on_step=True,
+    #             on_epoch=True,
+    #             logger=True,
+    #         )
+
+    #         # fig1, ax1 = plt.subplots()
+    #         # ax1.plot(self.betas, PAs)
+    #         # ax1.set_xlabel("beta")
+    #         # ax1.set_ylabel("PA")
+
+    #         # fig2, ax2 = plt.subplots()
+    #         # ax2.plot(self.betas, log_PAs)
+    #         # ax2.set_xlabel("Beta")
+    #         # ax2.set_ylabel("log_PA")
+    #         # logger = self.logger.experiment
+    #         # logger.log({"PA_vs_Beta":fig1})
+    #         # logger.log({"logPA_vs_Beta": fig2})
 
     def configure_optimizers(self):
         optimizer = self.hparams.optimizer(self.parameters())
