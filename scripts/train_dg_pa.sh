91c47c500a399d6530f82207c96ef885644cc972
Alessandro Torcinovich <torci.dev@gmail.com>
2023-08-12 10:53:36 +0200

    Add train_dg_pa.sh

---
+++ scripts/train_dg_pa.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+device=2,3,4,5
+process_size=1
+exp_names=("weak")
+process=0
+counter=0
+
+# CUDA_VISIBLE_DEVICES=${device} python src/train_dg_pa.py \
+#     experiment=dg/optimize_beta \
+#     data.dg.ds1_env=test0 \
+#     data.dg.ds2_env=test0 \
+#     model.dg.classifier.exp_name=diagvib_erm_weak \
+#     data.dg.shift_ratio=1 
+#     # trainer=ddp
+#     # logger=wandb
+
+for exp_name in "${exp_names[@]}"; do  # models
+    for env in $(seq 0 5); do
+        for shift_ratio in $(seq 0.1 0.1 1); do
+            log_file="dg_script_${counter}.log"
+            CUDA_VISIBLE_DEVICES=2,3,4,5 python src/train_dg_pa.py \
+                experiment=dg/optimize_beta \
+                data.dg.ds1_env=test0 \
+                data.dg.ds2_env=test${env} \
+                trainer.devices=4 \
+                model.dg.classifier.exp_name=diagvib_${exp_name} \
+                data.dg.shift_ratio=${shift_ratio} \
+                logger=wandb > "$log_file" 2>&1 &
+            
+            counter=$((counter+1))
+            process=$(((process+1)%$process_size))
+            if [ $process -eq 0 ]
+            then 
+                wait
+            fi
+        done
+    done
+done
