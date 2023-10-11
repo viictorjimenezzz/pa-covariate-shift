beb35a439f0c953d919a0f8e856e90084918bee2
aretor <torci.dev@gmail.com>
2023-10-11 15:17:14 +0200

    Change ticks

--- src/plot/adv/pgd.py
+++ src/plot/adv/pgd.py
@@ -79,7 +79,7 @@ def logpa(df: pd.DataFrame) -> None:
             ax.minorticks_on()
             if level == "linf":
                 ax.set_xticks(
-                    [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+                    [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
                 )
             else:  # level == "adversarial_ratio"
                 ax.set_xticks([0.0314, 0.0627, 0.1255])
