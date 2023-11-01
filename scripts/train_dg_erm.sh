9c72e71758ee470307d2a187f5c11bb05345582d
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2023-11-01 21:52:09 +0100

    Adapt ERM module to the new experimental setup. A suitable collate_fn adapts the output of the two-environment datamodule to the expected ERM batch.

---
+++ scripts/train_dg_erm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python src/train_dg.py \
+    --multirun \
+    data.dg.env1_name=env1 \
+    data.dg.env2_name=env2 \
+    experiment=dg/train_dg_erm \
+    trainer=ddp \
+    +trainer.fast_dev_run=False \
+    +trainer.limit_train_batches=1.0 \
+    +trainer.limit_val_batches=1.0 \
+    trainer.min_epochs=100 \
+    trainer.max_epochs=100 \
+    name=nan \
+    logger=none \
+    hydra/launcher=submitit_slurm \
+    hydra.launcher.tasks_per_node=1 \
+    hydra.launcher.mem_per_cpu=50000 \
+    +hydra.launcher.time=4:00:00 \
+    +hydra.launcher.num_gpus=4 \
+    logger.wandb.entity=malvai \
+    logger.wandb.project=cov_pa \
+    logger.wandb.group=ZSO_hue_3
+    name=erm_ndisj_ZSO_hue_3 \
---
+++ src/data/components/collate_functions.py
@@ -0,0 +1,76 @@
+import torch
+from torch import Tensor
+from typing import List
+
+
+"""
+This script contains collate functions for ERM, IRM and LISA for when data comes from different environments.
+By having them here, the same datamodule can be used for different models.
+"""
+
+def diagvib_collate_fn(batch: List, img_shape: Tensor = None):
+    aux = {}
+    for key in batch[0]:  # iterate over "first" and "second"
+        aux[key] = [
+            torch.cat(
+                [
+                    b[key][0].reshape(1, *img_shape) # self.dset1.img_shape
+                    for b in batch
+                ],
+                dim=0,
+            ),
+            torch.tensor([b[key][1] for b in batch]),
+        ]
+
+    # Save environments list
+    aux["env"] = [env for env in batch[0].keys()]
+    return aux
+
+def IRM_collate_fn(batch: List):
+
+    batch_dict = {}
+    for env in batch[0]:
+        batch_dict[env] = [
+            torch.stack([b[env][0] for b in batch]),
+            torch.tensor([b[env][1] for b in batch]),
+        ]
+
+    # Save environments list
+    batch_dict["envs"] = [env for env in batch[0].keys()]
+
+    return batch_dict
+    
+
+
+def ERM_collate_fn(batch: List):
+
+    if not isinstance(batch[0], dict): # just one environment
+        x = torch.stack([b[0] for b in batch])
+        y = torch.tensor([b[1] for b in batch])
+
+    else: # more than one environment
+        x = torch.stack([b[env][0] for env in batch[0] for b in batch])
+        y = torch.tensor([b[env][1] for env in batch[0] for b in batch])
+
+    return x, y
+
+
+def LISA_collate_fn(batch: List):
+    """
+    Prepares data for mixup and selective augmentation.
+    """
+
+    if not isinstance(batch[0], dict): # just one environment (only LISA-D)
+        x = torch.stack([b[0] for b in batch])
+        y = torch.tensor([b[1] for b in batch])
+        envs = ["0"]*len(batch)
+
+    else: # more than one environment
+        x = torch.stack([b[env][0] for env in batch[0] for b in batch])
+        y = torch.tensor([b[env][1] for env in batch[0] for b in batch])
+        envs = [env for env in batch[0] for b in batch]
+
+    return x, y, envs
+
+
+        
