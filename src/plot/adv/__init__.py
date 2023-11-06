299bfeb8ca476a2fea1d548263c2fa74f127a1bc
aretor <torci.dev@gmail.com>
2023-11-06 16:39:58 +0100

    Add general variables and AFR plotting

---
+++ src/plot/adv/__init__.py
@@ -0,0 +1,19 @@
+DASHES_DICT = {
+    "Standard": (2, 2),
+    "Engstrom2019Robustness": (2, 2),
+    "BPDA": (2, 2),
+    "Wong2020Fast": (2, 2),
+}
+COLORS_DICT = {
+    "Standard": "tab:orange",
+    "Engstrom2019Robustness": "tab:blue",
+    "BPDA": "tab:green",
+    "Wong2020Fast": "tab:red",
+}
+LABEL_DICT = {
+    "Standard": "Undefended",
+    "Engstrom2019Robustness": "Engstrom et al.",
+    "BPDA": "Athalye et al.",
+    "Wong2020Fast": "Wong et al.",
+}
+YEARS = [0, 2019, 2018, 2020]
