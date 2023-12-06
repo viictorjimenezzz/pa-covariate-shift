5bb71e779edbc3973a6fc723e1d590964f36f94e
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2023-12-06 15:30:46 +0100

    Readability + remove mean beta computation

--- src/models/PA_module.py
+++ src/models/PA_module.py
@@ -2,6 +2,7 @@ from typing import Optional, List, Any
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 
 import torch
 import torch.nn as nn
@@ -11,6 +12,14 @@ import pytorch_lightning as pl
 from src.models.components.post_agr import PosteriorAgreementKernel
 from torchmetrics.classification.accuracy import Accuracy
 
+from src.data.components import LogitsDataset
+from torch.utils.data import DataLoader, SequentialSampler
+
+#delete later
+from src.models.components.dg_backbone import DGBackbone
+import matplotlib.pyplot as plt
+from torchvision.transforms.functional import to_pil_image
+from PIL import Image
 
 class PosteriorAgreementModule(pl.LightningModule):
     """Optimization over the inverse temperature parameter of the Posterior Agreement kernel.
@@ -26,24 +35,29 @@ class PosteriorAgreementModule(pl.LightningModule):
 
         self.save_hyperparameters(logger=False)
 
-        for param in classifier.parameters():
+        self.model = classifier
+        for param in self.model.parameters():
             param.requires_grad = False
+        self.model.eval()
 
-        self.model = classifier.eval()
+        self.num_classes = 2
         self.kernel = PosteriorAgreementKernel(beta0=beta0)
-        self.afr_true = Accuracy(task="multiclass", num_classes=10) # FIX THIS LATER TOO
-        self.afr_pred = Accuracy(task="multiclass", num_classes=10)
+        self.afr_true = Accuracy(task="multiclass", num_classes=self.num_classes) # FIX THIS LATER TOO
+        self.afr_pred = Accuracy(task="multiclass", num_classes=self.num_classes)
+        self.acc_pa = Accuracy(task="multiclass", num_classes=self.num_classes)
 
-        # self.betas = np.linspace(0, 10, 100).tolist()
-        # self.training_step_outputs_1 = []
-        # self.training_step_outputs_2 = []
-
+        self.batch_size = 64 # default, changed later
+        self.logits_dataset = LogitsDataset(2) # two environments
+        self.betas = []
+        self.logPAs = []
+    
     def model_step(self, batch: dict):
         # QUICK FIX for projection. TODO: implement a better version
-
-        env_names = batch["envs"]
+        self.model.eval()
         self.kernel.beta.data.clamp_(min=0.0)
         self.kernel.reset()
+        
+        env_names = batch["envs"]
         x1, x2 = batch[env_names[0]][0], batch[env_names[1]][0]
 
         with torch.no_grad():
@@ -53,101 +67,63 @@ class PosteriorAgreementModule(pl.LightningModule):
 
         loss = -self.kernel.log_posterior()
         return o1, o2, loss
-
+    
     def training_step(self, train_batch: Any, batch_idx: int):
         o1, o2, loss = self.model_step(train_batch)
 
         if self.current_epoch == 0:  # AFR does not change during the epochs
+            if batch_idx == 0:
+                self.batch_size = o1.shape[0] # set batch size
             y_pred = torch.argmax(o1.data, 1)
             y_pred_adv = torch.argmax(o2.data, 1)
             y_true = train_batch["0"][1]
 
-            self.afr_pred(y_pred_adv, y_pred)
-            self.afr_true(y_pred_adv, y_true)
+            # First, store the logits as a dataset
+            self.logits_dataset.__additem__([o1, o2], y_true)
 
-            self.log(
-                "AFR pred",
-                self.afr_pred,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-            self.log(
-                "AFR true",
-                self.afr_true,
-                on_step=False,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-        # self.training_step_outputs_1.append(o1)
-        # self.training_step_outputs_2.append(o2)
-        self.kernel(o1, o2)
-
-        values = {
-            "beta": self.kernel.beta.item(),
-            "logPA": self.kernel.log_posterior().item(),
-            "PA": self.kernel.posterior(),
-        }
-
-        self.log_dict(
-            values,
-            prog_bar=True,
-            on_step=True,
-            on_epoch=True,
-            logger=True,
-        )
+            # Second, compute the AFR
+            values = {
+            "val/AFR pred": self.afr_pred(y_pred_adv, y_pred),
+            "val/AFR true": self.afr_true(y_pred_adv, y_true),
+            "val/acc_pa": self.acc_pa(torch.cat([y_pred, y_pred_adv]), torch.cat([y_true, y_true])),
+            }
+            self.log_dict(values, prog_bar=True, on_step=False, on_epoch=True, logger=True, sync_dist=True)
 
         return loss
 
-    # def on_train_epoch_end(self):
-    #     if self.current_epoch == 0:
-    #         PAs = []
-    #         log_PAs = []
-    #         for beta in self.betas:
-    #             kernel = PosteriorAgreementKernel(beta0=beta)
-    #             kernel.to(self.kernel.beta.device)
-    #             kernel.eval()
-    #             with torch.no_grad():
-    #                 for o1, o2 in zip(
-    #                     self.training_step_outputs_1,
-    #                     self.training_step_outputs_2,
-    #                 ):
-    #                     o1.to(kernel.beta.device)
-    #                     o2.to(kernel.beta.device)
-    #                     kernel(o1, o2)
-    #                 PAs.append(kernel.posterior().cpu().numpy())
-    #                 log_PAs.append(kernel.log_posterior().cpu().numpy())
+    def on_train_epoch_end(self):
+        beta_last = self.kernel.beta.item()
+        self.betas.append(beta_last)
 
-    #         values = {
-    #             "all_betas": self.betas,
-    #             "all_logPA": PAs,
-    #             "all_PA": log_PAs,
-    #         }
-    #         self.log_dict(
-    #             values,
-    #             prog_bar=True,
-    #             on_step=True,
-    #             on_epoch=True,
-    #             logger=True,
-    #         )
+        self.model.eval()
+        self.kernel.beta.data.clamp_(min=0.0)
+        self.kernel.reset()
+        logits_dataloader = DataLoader(dataset=self.logits_dataset,
+                                       batch_size=self.batch_size, # same as the data
+                                       num_workers=0, # we won't create subprocesses inside a subprocess, and data is very light
+                                       pin_memory=False, # only dense CPU tensors can be pinned
 
-    #         # fig1, ax1 = plt.subplots()
-    #         # ax1.plot(self.betas, PAs)
-    #         # ax1.set_xlabel("beta")
-    #         # ax1.set_ylabel("PA")
+                                       # Important so that it matches with the input data.
+                                       shuffle=False,
+                                       drop_last = False,
+                                       sampler=SequentialSampler(self.logits_dataset))
+        
+        for bidx, batch in enumerate(logits_dataloader):
+            logits, y = batch
+            self.kernel.evaluate(beta_last, logits[0], logits[1])
+                
+        # Retrieve final logPA for the (subset) batches
+        logPA = self.kernel.log_posterior()
+        self.logPAs.append(logPA.item())
 
-    #         # fig2, ax2 = plt.subplots()
-    #         # ax2.plot(self.betas, log_PAs)
-    #         # ax2.set_xlabel("Beta")
-    #         # ax2.set_ylabel("log_PA")
-    #         # logger = self.logger.experiment
-    #         # logger.log({"PA_vs_Beta":fig1})
-    #         # logger.log({"logPA_vs_Beta": fig2})
-
+        # Log the metrics
+        values = {
+            "val/beta": beta_last,
+            "val/logPA": logPA,
+            "val/PA": torch.exp(logPA),
+        }
+        self.log_dict(values, prog_bar=True, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        
     def configure_optimizers(self):
         optimizer = self.hparams.optimizer(self.parameters())
         return {"optimizer": optimizer}
