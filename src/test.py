d639e76e13a60d0573631a6d5ef953f858efb169
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2024-03-22 16:24:30 +0100

    Adjust checkpoint retrieval

--- src/test.py
+++ src/test.py
@@ -45,11 +45,31 @@ def test(cfg: DictConfig) -> Tuple[dict, dict]:
         Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
     """
 
+    """
+    - Same `experiment.name`, different `experiment_id` => different `seed`, different `ckpt_path` but track the same metric.
+    - Same `experiment.name`, same `experiment_id` => same `seed`, different `ckpt_path` because tracks another metric.
+    """
+
     # set seed for random number generators in pytorch, numpy and python.random
     pl.seed_everything(cfg.seed, workers=True)
 
+    # We load the experiment configuration: experiment_name, seed and metric.
+    path_ckpt_csv = cfg.paths.log_dir + "/ckpt_exp.csv"
+    experiment_df = pd.read_csv(path_ckpt_csv)
+    selected_ckpt = experiment_df[
+        (experiment_df['experiment_name'] == cfg.name_logger) & (experiment_df['seed'] == str(cfg.seed)) & (experiment_df['metric'] == cfg.checkpoint_metric)
+    ]
+    assert len(selected_ckpt) == 1, "There are duplicate experiments in the csv file."
+    
+    ckpt_path = selected_ckpt["ckpt_path"].item()
+    if 'wandb' in cfg.logger.keys():
+        cfg.logger.wandb.id = selected_ckpt['experiment_id'].item() # log in the same experiment
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+
     log.info(f"Instantiating datamodule <{cfg.data._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
 
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model: LightningModule = hydra.utils.instantiate(cfg.model)
@@ -58,9 +78,6 @@ def test(cfg: DictConfig) -> Tuple[dict, dict]:
     callbacks: List[Callback] = utils.instantiate_callbacks(
         cfg.get("callbacks")
     )
-
-    log.info("Instantiating loggers...")
-    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
     trainer: Trainer = hydra.utils.instantiate(
@@ -83,17 +100,13 @@ def test(cfg: DictConfig) -> Tuple[dict, dict]:
     # Test model
     log.info("\nStarting test!")
     trainer = hydra.utils.instantiate(
-        cfg.trainer, logger=logger, strategy='gpu', devices = 1
+        cfg.trainer, callbacks=callbacks, logger=logger
     )
 
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
     trainer.test(
-        model = model, # model.load_from_checkpoint(cfg.ckpt_path, net=hydra.utils.instantiate(cfg.model.net)), # because 'net' is not stored in the checkpoint
+        model = model.load_from_checkpoint(ckpt_path, net=hydra.utils.instantiate(cfg.model.net)),
         datamodule=datamodule,
     )
-
-    print("TESTING IS DONE, SHOW LENGTH TO DEBUG:")
-    print("lengths: ", torch.sum(model.len_test))
 
     test_metrics = trainer.callback_metrics
     metric_dict = {**test_metrics}
