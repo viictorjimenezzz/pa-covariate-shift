d56d08f0605c74bdb89ffc94895de6760a09524e
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2024-04-13 16:34:01 +0200

    Current `diagvib` modelselection script

--- scripts/dg_diagvib_train.sh
+++ scripts/dg_diagvib_train.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --mem-per-cpu=50G
-#SBATCH --time=4:00:00
+#SBATCH --mem-per-cpu=100G
+#SBATCH --time=24:00:00
 
 # activate conda env
 source activate $1
@@ -15,9 +15,10 @@ srun python3 src/train.py \
     callbacks=default_train_modelselection \
     +callbacks@callbacks.posterioragreement=pametric \
     +callbacks/components@callbacks.posterioragreement.dataset=pa_diagvib_val_modelselection \
-    +data/dg/diagvib/modelselection@diagvib_dataset=hue_idval_2 \
-    experiment=dg/diagvibsix/diagvibsix_erm \
-    +logger.wandb.name=erm_prova \
+    +data/dg/diagvib/modelselection@diagvib_dataset=hue_idval_2,hue_idval_10,hue_oodval_2,hue_oodval_10 \
+    experiment=dg/diagvibsix/diagvibsix_lisa \
+    +logger.wandb.name=lisa_10_10ep \
+    model.ppred=1.0\
     seed=0 \
     trainer=ddp \
     trainer.max_epochs=10 \
