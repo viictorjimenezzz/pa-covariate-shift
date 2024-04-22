390683f6b303324fa83a053afc8a16483d9ecbf9
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2024-04-22 19:17:49 +0200

    Change training parameters to increase performance

--- scripts/train_imagenet.sh
+++ scripts/train_imagenet.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=100G
 #SBATCH --time=120:00:00
 
 # activate conda env
@@ -19,6 +19,8 @@ srun python3 src/train.py \
     seed=0 \
     trainer=ddp \
     trainer.max_epochs=100 \
-    trainer.deterministic=true \
+    trainer.precision=16 \
+    trainer.deterministic=false \
+    data.num_workers=72 \
     +trainer.fast_dev_run=false \
     logger.wandb.name=train_palabelckpt \
