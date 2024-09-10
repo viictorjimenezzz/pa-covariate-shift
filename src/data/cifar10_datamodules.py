bdc42d10f306573067a4b5f482bd68b3d6b21d38
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2024-09-10 20:18:02 +0200

    Update data for adversarial setting

--- src/data/cifar10_datamodules.py
+++ src/data/cifar10_datamodules.py
@@ -4,6 +4,8 @@ import os.path as osp
 
 import torch
 from torch.utils.data import DataLoader, Dataset, TensorDataset
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
 
 from pytorch_lightning import LightningDataModule
 
@@ -11,38 +13,12 @@ from secml.data.loader import CDataLoaderCIFAR10
 from secml.ml.classifiers import CClassifierPyTorch
 from secml.adv.attacks import CAttack
 
-
 from src.data.components import MultienvDataset
 from src.data.components.cifar10_dataset import AdversarialCIFAR10Dataset
 from src.data.utils import carray2tensor
 from src.data.components.collate_functions import MultiEnv_collate_fn
 
 class CIFAR10DataModule(LightningDataModule):
-    """Example of LightningDataModule for MNIST dataset.
-
-    A DataModule implements 6 key methods:
-        def prepare_data(self):
-            # things to do on 1 GPU/TPU (not on every GPU/TPU in DDP)
-            # download data, pre-process, split, save to disk, etc...
-        def setup(self, stage):
-            # things to do on every process in DDP
-            # load data, set variables, etc...
-        def train_dataloader(self):
-            # return train dataloader
-        def val_dataloader(self):
-            # return validation dataloader
-        def test_dataloader(self):
-            # return test dataloader
-        def teardown(self):
-            # called on every process in DDP
-            # clean up after fit or test
-
-    This allows you to share a full dataset without explaining how to download,
-    split, transform and process the data.
-
-    Read the docs:
-        https://lightning.ai/docs/pytorch/latest/data/datamodule.html
-    """
 
     def __init__(
         self,
@@ -50,7 +26,7 @@ class CIFAR10DataModule(LightningDataModule):
         attack: CAttack,
         batch_size: int = 64,
         adversarial_ratio: float = 1.0,
-        data_dir: str = osp.join(".", "data", "datasets"),
+        dataset_dir: str = osp.join(".", "data", "datasets"),
         checkpoint_fname: str = "adversarial_data.pt",
         cache: bool = False,
         verbose: bool = False,
@@ -95,7 +71,7 @@ class CIFAR10DataModule(LightningDataModule):
             AdversarialCIFAR10Dataset(
                 self.attack,
                 self.cclassifier,
-                self.hparams.data_dir,
+                self.hparams.dataset_dir,
                 self.checkpoint_fname,
                 self.hparams.adversarial_ratio,
                 self.hparams.verbose,
@@ -103,11 +79,7 @@ class CIFAR10DataModule(LightningDataModule):
             )
 
     def setup(self, stage: Optional[str] = None):
-        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
 
-        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
-        careful not to execute things like random split twice!
-        """
         # load and split datasets only if not loaded already
         if not self.original_dset and not self.adversarial_dset:
             _, ts = CDataLoaderCIFAR10().load(val_size=0)
@@ -120,66 +92,57 @@ class CIFAR10DataModule(LightningDataModule):
             self.adversarial_dset = AdversarialCIFAR10Dataset(
                 self.attack,
                 self.cclassifier,
-                self.hparams.data_dir,
+                self.hparams.dataset_dir,
                 self.checkpoint_fname,
                 self.hparams.adversarial_ratio,
                 self.hparams.verbose,
                 self.hparams.cache,
             )
 
-            self.train_ds = MultienvDataset(
-                [self.original_dset, self.adversarial_dset]
-            )
+            self.train_dset_list = [self.original_dset, self.adversarial_dset]
 
     def train_dataloader(self):
-        return DataLoader(
-            dataset=self.train_ds,
-            batch_size=self.hparams.batch_size,
-            collate_fn=MultiEnv_collate_fn,
-            num_workers=self.hparams.num_workers,
-            pin_memory=self.hparams.pin_memory,
-            shuffle=False,
-        )
+        return {
+                str(i): DataLoader(
+                    dataset=ds,
+                    batch_size=self.hparams.batch_size,
+                    num_workers=self.hparams.num_workers,
+                    pin_memory=self.hparams.pin_memory,
+                    sampler = DistributedSampler(ds, drop_last=True, shuffle=True) if dist.is_initialized() else DistributedSampler(ds, drop_last=True, shuffle=True, num_replicas=1, rank=0),
+                ) for i, ds in enumerate(self.train_dset_list)
+        }
     
-    def val_dataloader(self):
-        return self.train_dataloader()
 
-    def teardown(self, stage: Optional[str] = None):
-        """Clean up after fit or test."""
-        pass
+from src.data.components import MultienvDataset
 
-    def state_dict(self):
-        """Extra things to save to checkpoint."""
-        return {}
-
-    def load_state_dict(self, state_dict: Dict[str, Any]):
-        """Things to do when loading checkpoint."""
-        pass
-
-
-from src.data.components.logits_pa import LogitsPA
-from src.pa_metric.pairing import PosteriorAgreementDatasetPairing
-
-class CIFAR10DataModulePA(CIFAR10DataModule):
+class CIFAR10DatasetPA(MultienvDataset):
     """
-    DataModule to use for PA optimization. 
+    Dataset with original and adversarial samples that will be fed to the Posterior
+    Agreement metric.
     """
-    def setup(self, stage: Optional[str] = None):
-        super().setup(stage)
-        self.train_ds = PosteriorAgreementDatasetPairing(self.train_ds)
 
-    def train_dataloader(self):
-        return DataLoader(
-            dataset=self.train_ds,
-            batch_size=self.hparams.batch_size,
-            collate_fn=MultiEnv_collate_fn,
-            num_workers=self.hparams.num_workers,
-            pin_memory=self.hparams.pin_memory,
-            shuffle=True, # We change shuffle to True, because we intend to optimize the PA.
+    def __init__(
+        self,
+        classifier: torch.nn.Module,
+        attack: CAttack,
+        adversarial_ratio: float = 1.0,
+        dataset_dir: str = osp.join(".", "data", "datasets"),
+        checkpoint_fname: str = "adversarial_data.pt",
+        cache: bool = True,
+        ):
+
+        cifar_dm = CIFAR10DataModule(
+            classifier=classifier,
+            attack=attack,
+            adversarial_ratio=adversarial_ratio,
+            dataset_dir=dataset_dir,
+            checkpoint_fname=checkpoint_fname,
+            cache=cache
         )
+        
+        # Since dataset is instantiated in a single process, I can call them here.
+        cifar_dm.prepare_data()
+        cifar_dm.setup()
 
+        super().__init__(cifar_dm.train_dset_list)
 
-class CIFAR10DataModulePAlogits(LogitsPA, CIFAR10DataModulePA):
-    def __init__(self, classifier: torch.nn.Module, *args, **kwargs):
-        super().__init__(classifier)
-        CIFAR10DataModulePA.__init__(self, classifier, *args, **kwargs)
