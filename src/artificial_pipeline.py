7bff793caea67365aa0902ddd296f1e8f0a2725e
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-02-10 17:58:23 +0100

    Make `artificial_pipeline.py` executable

--- src/artificial_pipeline.py
+++ src/artificial_pipeline.py
@@ -50,8 +50,6 @@ def test_artificial(prob: float) -> None:
         logits_dom2 = _generate_logits(preds_dom2)
 
         accuracy = _accuracy(torch.cat([y, y]), torch.cat([preds_dom1, preds_dom2]))
-        # print(f"\nACC '{classifier_type}': {accuracy:.2f}".format(accuracy=accuracy))
-
         pa_metric = PosteriorAgreementBase(
                 dataset = LogitsDataset([logits_dom1, logits_dom2], y),
                 pa_epochs = 50,
@@ -61,46 +59,7 @@ def test_artificial(prob: float) -> None:
 
         pa_metric.update(classifier=torch.nn.Identity())
         metric_dict = pa_metric.compute()
-        # print(f"logPA '{classifier_type}': {metric_dict['logPA']:.4f}".format(pa=metric_dict["logPA"]))
-        # print(f"beta: {metric_dict['beta']}")
     return metric_dict['beta'], metric_dict['logPA'], accuracy
 
-
-# if __name__ == "__main__":
-    """
-    Show as you increase N, that changing the probability yields the same PA for constant and
-    perfect (because they are robust), while not for the random one.
-
-    If accuracy = 0.7 (for instance), then you can divide the samples in two groups. One group with
-    paired aligned-misaligned containing 0.6*N of the samples (the 0.3*N wrong with some 0.3*N right), and
-    a group with the 0.4 right.
-
-    import torch
-    import torch.nn.functional as F
-
-    N = ??
-    beta = ??
-
-    probdist = F.softmax(beta * torch.tensor([[1.0, 0.0]]), dim=1)
-    delta = abs(probdist[0][0].item() - probdist[0][1].item())
-
-    EXAMPLE: N = 1000
-    p=0.4 >> acc = 0.53, beta=0.36, logPA = -690.82 >> ~ -0.94*N*delta**4 + 0.06*N*delta**2 = 0.95
-    p=0.8 >> acc = 0.69, beta=1.41, logPA = -619.17 >> ~ -0.62*N*delta**4 + 0.38*N*delta**2 = 55.8
-
-    Asi que la diferencia entre los dos deberia estar en ~ 55, y hemos obtenido ~ 70.
-
-    EXAMPLE: N = 100
-    p=0.2 >> acc = 0.62, beta=0.71, logPA = -68.59 >> ~ -0.76*N*delta**4 + 0.24*N*delta**2 = 1.76
-    p=0.8 >> acc = 0.72, beta=1.59, logPA = -59.3 >> ~ -0.56*N*delta**4 + 0.44*N*delta**2 = 8.53
-
-    Asi que la diferencia deberia estar cerca de ~7 y tenemos ~9.
-    """
-
-    # beta, logPA, accuracy = test_artificial(0.5)
-
-
-    # for prob in [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]:
-    # for prob in [0.5]:
-    #     print(f"\nProbability: {prob}")
-    #     test_artificial(prob)
+if __name__ == "__main__":
+    test_artificial(0.5)
